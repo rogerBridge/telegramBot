b62bd190956ce10a4d113c5ba929f3bbdfdf298d
rogerBridge <fenrmen@gmail.com>
2021-09-12 17:48:20 +0800

    rm rebuild.sh

--- rebuild.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-go build -o botmsg *.go;
-# docker stop botmsg && docker rm botmsg
-docker rmi rogerbridge/botmsg:test;
-docker build -t rogerbridge/botmsg:test .;
-docker push rogerbridge/botmsg:test;
